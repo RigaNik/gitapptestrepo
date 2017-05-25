62201f70ec750f9519e84e488c129767468bb660
dimiexperience <stefan.dimi91@gmail.com>
2017-05-25 13:25:37 -0400

    minor fixes that were bothering me:)

--- src/app/gitapp/git/git.component.ts
+++ src/app/gitapp/git/git.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {GitService} from "../git.service";
+import {GitService} from '../git.service';
 // import {Observable} from "rxjs/Observable";
 import {Repo} from "../repo.model";
 import {Router} from "@angular/router";
@@ -22,9 +22,14 @@ export class GitComponent implements OnInit {
     this.getLocal();
   }
 
-  public searchGit(search:string){
-    this._gitService.getRepos(search).subscribe(data=>
-      data.items.forEach(item=>{
+  public clearLocalCache() {
+    window.localStorage.clear();
+    this.ngOnInit();
+  }
+
+  public searchGit(search: string) {
+    this._gitService.getRepos(search).subscribe(data =>
+      data.items.forEach(item => {
         let list_item = new Repo(item.id, item.name, item.stargazers_count, item.created_at, item.owner.login, item.owner.avatar_url, item.full_name, item.description, item.language, item.forks, item.watchers, item.clone_url);
         this.result.push(list_item);
       })
@@ -35,9 +40,9 @@ export class GitComponent implements OnInit {
     this._router.navigate(['/', item.owner, item.name]);
   }
 
-  private getLocal(){
-    for(let key in window.localStorage){
-      if(key.indexOf('repo') != -1){
+  private getLocal() {
+    for (let key in window.localStorage) {
+      if (key.indexOf('repo') !== -1) {
         this.localResults.push(JSON.parse(window.localStorage.getItem(key)));
       }
     }
